58d10e49a36f0ad2228c278f2b8eef87f03ac847
Mohamed <ms7237322@gmail.com>
2026-08-21 16:27:46 +0300

    hero slider images

--- src/components/website/home/Hero/data.ts
+++ src/components/website/home/Hero/data.ts
@@ -1,15 +1,15 @@
 export const heroCards = [
-	{ id: 1, title: 'مطاعم', img: '/restaurants.png' },
-	{ id: 2, title: 'مطاعم بحرية', img: '/seafood.png' },
-	{ id: 3, title: 'كافيهات', img: '/cafes.png' },
-	{ id: 4, title: 'فاست فود وساندويتشات', img: '/fast-food.png' },
-	{ id: 5, title: 'مطاعم شعبية', img: '/traditional-restaurants.png' },
-	{ id: 6, title: 'حلويات ومخابز', img: '/sweets-bakeries.png' },
-	{ id: 7, title: 'معالم سياحية', img: '/tourist-attractions.png' },
-	{ id: 8, title: 'شواطئ وكورنيش', img: '/beaches-corniche.png' },
-	{ id: 9, title: 'فنادق', img: '/hotels.png' },
-	{ id: 10, title: 'مولات ومراكز تسوق', img: '/malls.png' },
-	{ id: 11, title: 'حدائق ومتنزهات', img: '/parks.png' },
+	{ id: 1, title: 'مطاعم بحرية', img: '/hero-slider-images/seafood.jpg' },
+	{ id: 2, title: 'مطاعم', img: '/hero-slider-images/restaurants.jpg' },
+	{ id: 3, title: 'كافيهات', img: '/hero-slider-images/cafes.jpg' },
+	{ id: 4, title: 'فاست فود وساندويتشات', img: '/hero-slider-images/fast-food.jpg' },
+	{ id: 5, title: 'مطاعم شعبية', img: '/hero-slider-images/traditional-restaurants.jpg' },
+	{ id: 6, title: 'حلويات ومخابز', img: '/hero-slider-images/sweets-bakeries.jpg' },
+	{ id: 7, title: 'معالم سياحية', img: '/hero-slider-images/elola.jpg' },
+	{ id: 8, title: 'شواطئ وكورنيش', img: '/hero-slider-images/beaches-corniche.jpg' },
+	{ id: 9, title: 'فنادق', img: '/hero-slider-images/hotels.jpg' },
+	{ id: 10, title: 'مولات ومراكز تسوق', img: '/hero-slider-images/malls.jpg' },
+	{ id: 11, title: 'حدائق ومتنزهات', img: '/hero-slider-images/parks.jfif' },
 ];
 
 export const heroStats = [
