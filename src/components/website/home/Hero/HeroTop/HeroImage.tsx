ce386bb97fc087577f1d9af5cfbcd5cd32bf32bd
Mohamed <ms7237322@gmail.com>
2026-08-26 19:26:30 +0300

    style(hero): scale down hero profile image

--- src/components/website/home/Hero/HeroTop/HeroImage.tsx
+++ src/components/website/home/Hero/HeroTop/HeroImage.tsx
@@ -4,9 +4,9 @@ import HeroStats from './HeroStats'
 
 export default function HeroImage() {
   return (
-    <div className="flex flex-col items-center gap-6 ps-2">
+    <div className="flex flex-col items-center gap-6 ps-6">
       <div className="relative z-10">
-        <Image src="/mohamedwhite1.png" alt="profile" width={320} height={320} className="rounded-xl object-cover" />
+        <Image src="/mohamedwhite1.png" alt="profile" width={290} height={320} className="rounded-xl object-cover" />
       </div>
 
     <div className="w-full px-2 flex flex-col items-center gap-3 -mt-32 z-20 relative">
