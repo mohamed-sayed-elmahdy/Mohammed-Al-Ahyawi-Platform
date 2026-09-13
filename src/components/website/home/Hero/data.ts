b2f88772748663af38e8fd3081e96a723a48ba0e
Mohamed <ms7237322@gmail.com>
2026-09-13 19:26:49 +0300

    content: update hero view count to 75 million

--- src/components/website/home/Hero/data.ts
+++ src/components/website/home/Hero/data.ts
@@ -14,7 +14,7 @@ export const heroCards = [
 
 export const heroStats = [
 	{ id: 1, value: '+1300', label: 'تقييم وتجربة موثقة' },
-	{ id: 2, value: '+70M', label: "مشاهدة" },
+	{ id: 2, value: '+75M', label: "مشاهدة" },
 	// { id: 3, value: '5+', label: 'دول حول العالم' },
 ];
 
